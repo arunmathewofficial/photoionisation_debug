761e119ea759866291c455071e9bd9a98369b62c
mathew <a.mathew@iitg.ac.in>
2023-10-16 09:37:45 +0100

    updating

--- silo/IonisationProfile.py
+++ silo/IonisationProfile.py
@@ -18,17 +18,18 @@ for frame in timeline:
     basic_data = object.get_basic_data()
 
     # density = object.get_parameter('Density')
-    #O1p = object.get_parameter('Tr025_O1p')/object.get_parameter('Tr004_X_O')
-    #O2p = object.get_parameter('Tr026_O2p') / object.get_parameter('Tr004_X_O')
-    #O3p = object.get_parameter('Tr027_O3p') / object.get_parameter('Tr004_X_O')
-    #O4p = object.get_parameter('Tr028_O4p') / object.get_parameter('Tr004_X_O')
-    #O5p = object.get_parameter('Tr029_O5p') / object.get_parameter('Tr004_X_O')
-    #O6p = object.get_parameter('Tr029_O5p') / object.get_parameter('Tr004_X_O')
-    #O0 = np.ones_like(O1p) - O1p - O2p - O3p - O4p - O5p - O6p
-
-
     H1p = object.get_parameter('Tr001_H1p')/object.get_parameter('Tr000_X_H')
     H0 = np.ones_like(H1p) - H1p
+
+    '''
+    O1p = object.get_parameter('Tr025_O1p')/object.get_parameter('Tr004_X_O')
+    O2p = object.get_parameter('Tr026_O2p') / object.get_parameter('Tr004_X_O')
+    O3p = object.get_parameter('Tr027_O3p') / object.get_parameter('Tr004_X_O')
+    O4p = object.get_parameter('Tr028_O4p') / object.get_parameter('Tr004_X_O')
+    O5p = object.get_parameter('Tr029_O5p') / object.get_parameter('Tr004_X_O')
+    O6p = object.get_parameter('Tr029_O5p') / object.get_parameter('Tr004_X_O')
+    O0 = np.ones_like(O1p) - O1p - O2p - O3p - O4p - O5p - O6p
+    '''
 
     radius = object.get_radial_coordinate()
 
